3d2a17a3ef03548e381568b60c6d17b348291bf4
Chris Resnick <chris.resnick@gmail.com>
2020-10-30 17:41:29 -0400

    initil setup

---
+++ db/migrations/20201030213019-create-user.js
@@ -0,0 +1,38 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      username: {
+        type: Sequelize.STRING(50),
+        allowNull:false,
+        unique: true
+      },
+      password: {
+        type: Sequelize.STRING.BINARY,
+        allowNull: false
+      },
+      email: {
+        type: Sequelize.STRING(100),
+        unique: true,
+        allowNull: false
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Users');
+  }
+};
