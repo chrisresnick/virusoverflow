3734dc905b5f18526a117de1d11a1967a3dd9090
Chris Resnick <chris.resnick@gmail.com>
2020-11-02 15:48:23 -0500

    starting auth and updated db

--- routes/index.js
+++ routes/index.js
@@ -1,26 +1,30 @@
 var express = require('express');
 var router = express.Router();
 const {asyncHandler} = require("./utils");
-const {Users} = require("../db/models/index");
-const bcryt = require("bcryptjs");
+const {User} = require("../db/models/index");
+const bcrypt = require("bcryptjs");
+const session = require('express-session');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'a/A Express Skeleton Home' });
-});
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'a/A Express Skeleton Home' });
+// });
 
 router.get("/login", (req, res) => {
   res.render("login");
 })
 
-router.post('login', asyncHandler(async (req, res) => {
+router.post('/login', asyncHandler(async (req, res) => {
   const {username, password} = req.body;
-  const user = await Users.findOne({where:username});
+  const user = await User.findOne({where:{username}});
   if(!user){
     //handle username not found
   }
-  if(bcrypt.compare(password, user.password)){
-    //issue a toke
+  if(await bcrypt.compare(password, user.password.toString())){
+    req.session.auth = {
+      userId: user.id,
+    };
+
   }
   else {
     //handle wrong password
