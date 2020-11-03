5d66347d961bfec170a455edd0a19562dec39659
Mathias <mathiasanderson88@gmail.com>
2020-11-02 16:30:09 -0800

    Migrate Question Table

---
+++ db/migrations/20201103002254-create-question.js
@@ -0,0 +1,32 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Questions', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      userId: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      textArea: {
+        allowNull: false,
+        type: Sequelize.TEXT
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Questions');
+  }
+};
---
+++ db/models/question.js
@@ -0,0 +1,30 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Question extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  Question.init({
+    userId: {
+      allowNull: false,
+      type: DataTypes.INTEGER,
+    },
+    textArea: {
+      allowNull: false,
+      type: DataTypes.TEXT
+    }
+  }, {
+    sequelize,
+    modelName: 'Question',
+  });
+  return Question;
+};
