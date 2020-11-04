cb80d5a4935345e2ac1b49836b2f66fb4ceebd2c
Chris Resnick <chris.resnick@gmail.com>
2020-11-04 17:47:34 -0500

    fetch vote counts

--- routes/answer.js
+++ routes/answer.js
@@ -1,22 +1,26 @@
 const express = require("express");
 const router = express.Router();
+const {AnswerVotes} = require("../db/models/index");
+const {requireAuth, asyncHandler} = require("./utils");
 
 async function ansVoteSum(answerId){
-    let votes = await AnswerVote.findAll({where:{answerId}})
+    let votes = await AnswerVotes.findAll({where:{answerId}})
     votes = votes.map(vote => vote.toJSON())
      return votes.reduce((acc, vote) => {
         return acc + (vote.isUpVote ? 1 : -1);
     }, 0);
 }
-
-router.post("/answer/:id(\\d+)/vote", requireAuth, asyncHandler(async(req, res) => {
+router.get("/:id(\\d+)/vote", asyncHandler(async(req, res) => {
+    res.json({count: await ansVoteSum(req.params.id)})
+}));
+router.post("/:id(\\d+)/vote", requireAuth, asyncHandler(async(req, res) => {
 
     const answerId = req.params.id;
     const {isUpVote} = req.body
     const userId = res.locals.user.id;
-    const existingVote = await AnswerVote.findOne({where:{answerId, userId}})
+    const existingVote = await AnswerVotes.findOne({where:{answerId, userId}})
     if(!existingVote){ //if the user hasnt voted on this question yet, create a new vote
-        const vote = await AnswerVote.create({answerId, userId, isUpVote});
+        const vote = await AnswerVotes.create({answerId, userId, isUpVote});
     }
     else if(existingVote.isUpVote != isUpVote) { //change the vote
         existingVote.isUpVote = isUpVote;
