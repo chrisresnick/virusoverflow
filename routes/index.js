c140c661f6a6fa4f64e0c841fbb3805dd64e2201
Chris Resnick <chris.resnick@gmail.com>
2020-11-05 14:09:22 -0500

    search backend works

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 const { asyncHandler } = require("./utils");
-const { User } = require("../db/models/index");
+const { User, Question, Answer } = require("../db/models/index");
 
 const bcrypt = require("bcryptjs");
 const session = require('express-session');
+const sw = require("stopword");
+const Op = require("sequelize").Op
 const { check, validationResult } = require("express-validator");
 
 /* GET home page. */
@@ -67,6 +69,41 @@ router.post('/login', loginValidator, asyncHandler(async (req, res) => {
   else {
     return res.render("login", { errors: ["Username and Password Combination not valid"] })
   }
+}));
+
+router.post("/search", asyncHandler(async (req, res) => {
+  const words = sw.removeStopwords(req.body.searchTerm.split(" "));
+  const re = words.map(word => `%${word}%`);
+  console.log("re", re);
+  const results = {}
+  for(let term of re) {
+    console.log("term", term);
+    let questions = await Question.findAll({where:{textArea: {[Op.iLike]: term}}})
+    console.log("questions:", questions);
+    let answers = await Answer.findAll({where:{textField: {[Op.iLike]: term}}})
+    console.log("answers:", answers);
+    questions.forEach(question => {
+      if(!(question.id in results)) results[question.id] = {count:0, question};
+      results[question.id].count++;
+    });
+    answers.forEach(async answer => {
+        console.log("questionId", answer.questionId)
+        if(!(answer.questionId in results)){
+          let thisQues = await Question.findByPk(answer.questionId);
+          results[answer.questionId] = {count: 0, question: thisQues};
+        };
+        results[answer.questionId].count++;
+    });
+  }
+  const releventQuestions = Object.keys(results);
+  releventQuestions.sort((a,b) => {
+    const aVal = results[a].count;
+    const bVal = results[b].count;
+    if(aVal===bVal) return 0;
+    return aVal > bVal ? -1 : 1;
+  });
+  const questions = releventQuestions.map(q => results[q].question);
+  res.json({questions})
 }));
 
 module.exports = router;
