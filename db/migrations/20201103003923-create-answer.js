7054ee079dc7a7b6aa2e67548c0b5b46a4785014
Saad <saad_shaikh@aol.com>
2020-11-08 10:10:19 -0500

    minor bug fixes2

--- db/migrations/20201103003923-create-answer.js
+++ db/migrations/20201103003923-create-answer.js
@@ -1,38 +1,38 @@
-'use strict';
+"use strict";
 module.exports = {
-  up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('Answers', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      questionId: {
-        allowNull: false,
-        type: Sequelize.INTEGER,
-        references: { model: "Questions"}
-      },
-      userId: {
-        allowNull: false,
-        type: Sequelize.INTEGER,
-        references: { model: "Users"}
-      },
-      textField: {
-        allowNull: false,
-        type: Sequelize.TEXT
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
-  },
-  down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('Answers');
-  }
+	up: async (queryInterface, Sequelize) => {
+		await queryInterface.createTable("Answers", {
+			id: {
+				allowNull: false,
+				autoIncrement: true,
+				primaryKey: true,
+				type: Sequelize.INTEGER,
+			},
+			questionId: {
+				allowNull: false,
+				type: Sequelize.INTEGER,
+				references: { model: "Questions" },
+			},
+			userId: {
+				allowNull: false,
+				type: Sequelize.INTEGER,
+				references: { model: "Users" },
+			},
+			textField: {
+				allowNull: false,
+				type: Sequelize.TEXT,
+			},
+			createdAt: {
+				allowNull: false,
+				type: Sequelize.DATE,
+			},
+			updatedAt: {
+				allowNull: false,
+				type: Sequelize.DATE,
+			},
+		});
+	},
+	down: async (queryInterface, Sequelize) => {
+		await queryInterface.dropTable("Answers");
+	},
 };
