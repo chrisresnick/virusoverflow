2b512b7366287ea498da1b6993d3c350a400f5e5
Kerri <gant.kerri@gmail.com>
2020-11-09 01:43:22 -0500

    Touched up the CSS to make pages match more

--- db/seeders/20201103233419-Answers.js
+++ db/seeders/20201103233419-Answers.js
@@ -41,11 +41,7 @@ module.exports = {
   },
 
   down: async (queryInterface, Sequelize) => {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+    await queryInterface.bulkDelete('Answers', null, {});
+
   }
 };
